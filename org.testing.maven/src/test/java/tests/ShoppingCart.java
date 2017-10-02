6e22c0dfde84e95cd0e02f90b5716df7b611e2f2
jose.batista <jose.batista@LSTKEN100434.softtekclients.com>
2017-10-02 11:50:43 -0700

    set Bin

--- org.testing.maven/src/test/java/tests/ShoppingCart.java
+++ org.testing.maven/src/test/java/tests/ShoppingCart.java
@@ -24,8 +24,8 @@ public class ShoppingCart extends SeleniumMethods{
 		test = extent.createTest("Shopping Cart");
 		
 		// Setting Chromedriver binary directory
-		//ChromeOptions options = new ChromeOptions();
-		//options.setBinary(new File(".\\src\\test\\java\\tests\\data\\chromedriver.exe"));
+		ChromeOptions options = new ChromeOptions();
+		options.setBinary(new File("C:\\Users\\jose.batista\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe"));
 		
 		
 		// Create Webdriver instances
@@ -34,7 +34,7 @@ public class ShoppingCart extends SeleniumMethods{
 		//cap.setPlatform(Platform.WINDOWS);
 		//cap.setBrowserName("chrome");
 		//WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.67:7777/wd/hub"), cap);
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver(options);
 		//driver.manage().deleteAllCookies();
 		WebDriverWait waitForObject = new WebDriverWait(driver, 5);
 		
